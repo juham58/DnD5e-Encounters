8492293d1ebb99d24ec0f9b10da008c94a32ec1d
juham58 <59977027+juham58@users.noreply.github.com>
2021-02-18 23:51:38 -0500

    Simulation de combat de base done

--- Initiative_Module.py
+++ Initiative_Module.py
@@ -5,13 +5,17 @@ from pathlib import Path
 class Initiative_Module():
     def __init__(self):
         self.combatants_stats = {}
+        self.combatants_hp = {}
         self.combatants_names = []
+        self.players_names = []
+        self.monsters_names = []
         self.ini_order = {}
         self.verbose = True
 
     def import_stats(self, name):
         stats = pickle.load(open(Path.cwd()/"data"/"stats_{}".format(name), "rb"))
         self.combatants_stats[name] = stats[name]
+        self.combatants_hp[name] = stats[name]["hp"]
         self.combatants_names.append(name)
 
     def import_group(self, base_name, quantity):
@@ -20,6 +24,7 @@ class Initiative_Module():
             new_name = "{}_{}".format(base_name, n+1)
             new_dict = stats[base_name]
             self.combatants_stats[new_name] = new_dict
+            self.combatants_hp[new_name] = new_dict["hp"]
             self.combatants_names.append(new_name)
 
     def d20(self):
@@ -29,34 +34,99 @@ class Initiative_Module():
         temp_dict = {}
         for name in self.combatants_names:
             temp_dict[name] = self.d20()+self.combatants_stats[name]["ini_mod"]
-        self.ini_order = dict(sorted(temp_dict.items(), key=lambda item: item[1], reverse=True))
+        self.ini_order = list(dict(sorted(temp_dict.items(), key=lambda item: item[1], reverse=True)).keys())
+
+    def separate_players_vs_monsters(self):
+        for name in self.combatants_names:
+            if self.combatants_stats[name]["is_monster"] is True:
+                self.monsters_names.append(name)
+            else:
+                self.players_names.append(name)
 
     def attack(self, attacker_name, target_name):
         attack_roll = self.d20()+self.combatants_stats[attacker_name]["attack_mod"]
         straight_roll = attack_roll-self.combatants_stats[attacker_name]["attack_mod"]
         if straight_roll == 20:
-            self.combatants_stats[target_name]["hp"] -= 2*self.combatants_stats[attacker_name]["avg_attack_dmg"]
+            self.combatants_hp[target_name] -= 2*self.combatants_stats[attacker_name]["avg_attack_dmg"]
             if self.verbose is True:
                 print(attacker_name, " CRITS with ", attack_roll, " and does: ", self.combatants_stats[attacker_name]["avg_attack_dmg"], " damage!")
 
         if attack_roll >= self.combatants_stats[target_name]["ac"] and straight_roll != 20:
-            self.combatants_stats[target_name]["hp"] -= self.combatants_stats[attacker_name]["avg_attack_dmg"]
+            self.combatants_hp[target_name] -= self.combatants_stats[attacker_name]["avg_attack_dmg"]
             if self.verbose is True:
                 print(attacker_name, " hits with ", attack_roll, " and does: ", self.combatants_stats[attacker_name]["avg_attack_dmg"], " damage!")
         else:
             if self.verbose is True:
                 print(attacker_name, " misses.")
 
-    def set_target(self, attacker_name):
+    def heal(self):
         pass
+
+    def set_target(self, attacker_name):
+        if self.combatants_stats[attacker_name]["is_monster"] is True:
+            return random.choice(self.players_names)
+
+        else:
+            return random.choice(self.monsters_names)
+
+    def death(self, name):
+        if self.combatants_stats[name]["is_monster"] is True:
+            self.monsters_names.remove(name)
+            self.ini_order.remove(name)
+
+        else:
+            self.players_names.remove(name)
+            self.ini_order.remove(name)
+
+    def check_for_death(self):
+        if any(v <= 0 for v in self.combatants_hp.values()):
+            dead_list = []
+            for name in self.combatants_hp:
+                if self.combatants_hp[name] <= 0:
+                    self.death(name)
+                    dead_list.append(name)
+            for name in dead_list:
+                print(name, " dies.")
+                del self.combatants_hp[name]
+
+    def player_downed(self):
+        pass
+
+    def combat(self, verbose=True):
+        rounds = 1
+        self.roll_ini()
+        self.separate_players_vs_monsters()
+        while len(self.players_names) != 0 and len(self.monsters_names) != 0:
+            for attacker_name in self.ini_order:
+                for n in range(self.combatants_stats[attacker_name]["number_of_attacks"]):
+                    target = self.set_target(attacker_name)
+                    self.attack(attacker_name, target)
+                    self.check_for_death()
+                    #print("\n Combatants HP:", self.combatants_hp)
+                    if len(self.players_names) == 0 or len(self.monsters_names) == 0:
+                        break
+            rounds += 1
+        print("Combat ended")
+        if len(self.players_names) == 0:
+            print("The players were killed.")
+            return 0
+        else:
+            print("The monsters were killed.")
+            return 1
+                
+
 
 
 # À FAIRE:
-# Besoin d'une manière de séparer les monstres des joueurs et les frontliners
-# Besoin d'une manière de décider qui est attaqué
+# Besoin d'une manière de séparer les monstres des joueurs (check)
+# Besoin d'une manière de décider qui est attaqué (Check: random pour l'instant)
+# Besoin d'une manière de distinguer les frontliners (facultatif)
 # Faire l'algorithme de simulation de combat
-# Retirer des combatants morts
+# Vérifier s'il y a des morts
+# Gérer les death saves
+# Retirer les combatants morts (check)
     
 test = Initiative_Module()
 test.import_group("Goblin", 8)
-test.roll_ini()
+test.import_stats("John")
+test.combat()
--- Stats_Module.py
+++ Stats_Module.py
@@ -10,19 +10,21 @@ class MainStats():
         self.hp = 25
         self.ini_mod = 0
         self.attack_mod = 0
+        self.number_of_attacks = 1
         self.is_monster = True
         self.is_frontliner = True
 
     def add_avg_dmg(self, x, y, z):
         self.avg_attack_dmg += round(x*((y+1)/2)+z)
 
-    def set_main_stats(self, name, ac=10, hp=25, dc=10, ini_mod=0, attack_mod=0, is_monster=True, is_frontliner=True):
+    def set_main_stats(self, name, ac=10, hp=25, dc=10, ini_mod=0, attack_mod=0, number_of_attacks=1, is_monster=True, is_frontliner=True):
         self.name = name
         self.ac = ac
         self.hp = hp
         self.dc = dc
         self.ini_mod = ini_mod
         self.attack_mod = attack_mod
+        self.number_of_attacks = number_of_attacks
         self.is_monster = is_monster
         self.is_frontliner = is_frontliner
     
@@ -34,6 +36,7 @@ class MainStats():
                 "hp": self.hp,
                 "ini_mod": self.ini_mod,
                 "attack_mod": self.attack_mod,
+                "number_of_attacks": self.number_of_attacks,
                 "is_monster": self.is_monster,
                 "is_frontliner": self.is_frontliner}}
         pickle.dump(dict, open(Path.cwd()/"data"/"{}_{}".format("stats", self.name), "w+b"))
@@ -41,4 +44,9 @@ class MainStats():
 test = MainStats()
 test.set_main_stats("Goblin", ac=15, hp=7, attack_mod=4)
 test.add_avg_dmg(1, 6, 2)
+test.save_main_stats()
+
+test.set_main_stats("John", ac=17, hp=83, dc=15, ini_mod=5, attack_mod=10, number_of_attacks=2, is_monster=False)
+test.add_avg_dmg(1, 8, 8)
+test.add_avg_dmg(1, 6, 0)
 test.save_main_stats()
