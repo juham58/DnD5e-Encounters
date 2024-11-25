cdf250a798fb4585e23c4715e13eea1526184fc8
Justin Hamel <59977027+juham58@users.noreply.github.com>
2024-11-25 16:22:50 -0500

    Lours + beefed Worg

--- EncounterSim/analysis.py
+++ EncounterSim/analysis.py
@@ -73,7 +73,7 @@ def combat_analysis(iterations, monsters_list, list_of_players, verbose=False, m
 #combat_analysis(500, ["Master Brawler"], ["Gwenyth", "Kal", "Kara", "Denis", "Ghaz"], verbose=False)
 #combat_analysis(5000, ["Grenat", "Cinabre", "Vermillon"], ["Gwenyth", "Kal", "Kara", "Denis", "Ghaz", "Cornelia"], verbose=False)
 #combat_analysis(5000, ["Zoldane Vitruve"], ["Gwenyth", "Kal", "Kara", "Denis", "Ghaz", "Cornelia"], verbose=False)
-combat_analysis(2500, ["Bes"], ["Gwenyth", "Kal", "Kara", "Denis"], verbose=False)
+#combat_analysis(2500, ["Bes"], ["Gwenyth", "Kal", "Kara", "Denis", "Ghaz", "Cornelia"], verbose=False)
 
 def monsters_test(iterations, monster_name, number_of_monsters, list_of_players, list_of_monsters_to_import=[], verbose=False):
     total_start_time = time.process_time()
@@ -175,7 +175,7 @@ def monsters_test(iterations, monster_name, number_of_monsters, list_of_players,
 #monsters_test(50, "Zombtrouille", 100, ["Gwenyth", "Kal", "Kara", "Denis", "Ghaz", "Illian"])
 #monsters_test(100, "Night Scavver", 10, ["Gwenyth", "Kal", "Kara", "Denis", "Ghaz"])
 #monsters_test(100, "Husk Zombie", 15, ["Gwenyth", "Kal", "Kara", "Denis", "Ghaz"], list_of_monsters_to_import=["Revenant"])
-#monsters_test(50, "Chimera", 10, ["Gwenyth", "Kal", "Kara", "Denis", "Ghaz"])
+monsters_test(50, "Lours", 25, ["Gwenyth", "Kal", "Kara", "Denis", "Ghaz"])
 
 #monsters_test(50, "Skeleton", 150, ["John", "Faramir", "Augustin", "Rand al'Thor", "Victoriana", "Dorran"])
 #monsters_test(300, "Thug", 10, ["Ewyn", "Gowon", "Iaachus", "Melvin", "Reaghan", "Vilgefortz"])
